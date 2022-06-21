c82210738c456986da686166c9331b66bbdc00dc
a-d-i-t-y-a-5-7 <adityadeshpande311098@gmail.com>
2022-06-21 11:52:44 +0530

    Multiplication Removed

--- Calculator.java
+++ Calculator.java
@@ -10,7 +10,7 @@ public class Calculator {
 		int choice;
 		do
 		{
-			System.out.println("1.Add 2.Subtract 3.Multiply 4.Division 5.Exit");
+			System.out.println("1.Add 2.Subtract 3.Division 4.Exit");
 			choice = s.nextInt();
 			switch(choice)
 			{
@@ -37,15 +37,6 @@ public class Calculator {
 				num1 = s.nextInt();
 				System.out.println("Enter second number");
 				num2 = s.nextInt();
-				int mul = num1 * num2;
-				System.out.println("The Multiplication is: "+mul);
-				break;
-				
-			case 4:
-				System.out.println("Enter first number");
-				num1 = s.nextInt();
-				System.out.println("Enter second number");
-				num2 = s.nextInt();
 				if(num2>0) {
 					int div = num1/num2;
 					System.out.println("The division is: "+div);
@@ -54,13 +45,13 @@ public class Calculator {
 					System.out.println("Denominator can't be 0");
 				break;
 				
-			case 5:
+			case 4:
 				break;
 				
 			default:
 				System.out.println("Invalid input");
 			}
-		}while(choice!=5);
+		}while(choice!=4);
 	}
 
 }
